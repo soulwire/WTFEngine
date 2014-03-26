c552b2698dc9c8f561ba5f747da9ba3d905c6e50
pikesley <github@orgraphone.org>
2014-03-26 15:29:19 +0000

    Oh I'm an idiot

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -36,6 +36,6 @@ $(function() {
 
     // Populate using a Google spreadsheet ID (you must publish it first!)
     // @see https://support.google.com/drive/answer/37579?hl=en
-    WTF.init( '0AvG1Hx204EyydF9ub1M2cVJ3Z1VGdDhTSWg0ZV9LNGc' );
+    WTF.init( '0AvyECrEnW-a1dHp5TFg4T1RielRfZ1lSTDhWOFhlM0E' );
 
 });
